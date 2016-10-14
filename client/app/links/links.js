1d1d9bbade414689b42757ee6550648039d823d8
tgaribal <tgaribal@gmail.com>
2016-10-14 15:18:11 -0700

    Complete Links factory section

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,20 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+
+  $scope.getAll = function() {
+    Links.getAll($scope.data)
+    .then(function() {
+      console.log('Links in controller:', Links);
+    });
+    // return $scope.data = Links;
+  };
+
+  $scope.addOne = function() {
+    Links.addOne($scope.data)
+    .then(function() {
+      console.log('Addone in controller!');
+    });
+  };
 });
