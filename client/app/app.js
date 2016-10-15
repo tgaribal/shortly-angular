fbd65f548e42e1229d16592c4de45d37b8de3775
tgaribal <tgaribal@gmail.com>
2016-10-15 11:52:56 -0700

    Complete user validation

--- client/app/app.js
+++ client/app/app.js
@@ -9,7 +9,7 @@ angular.module('shortly', [
   $routeProvider
     .when('/signin', {
       templateUrl: 'app/auth/signin.html',
-      controller: 'AuthController'
+      controller: 'AuthController',
     })
     .when('/signup', {
       templateUrl: 'app/auth/signup.html',
@@ -17,11 +17,13 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     });
 
     // We add our $httpInterceptor into the array
